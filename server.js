7fb7811c5520c4e17bc7ee5203e5c966519e1f09
safadigital <safarov@safadigital.su>
2025-02-15 10:49:46 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+// import express from "express";
+// import multer from "multer";
+// import ffmpeg from "fluent-ffmpeg";
+// import path from "path";
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+
+const express = require('express');
+const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+const fs = require('fs').promises;
+
+const timeout = require('connect-timeout');
+
+// function to de;ete a;; fi;es inside directory
+async function removeRecursively(directory) {
+  await fs.rmdir(directory, { recursive: true });
+}
+
+// Call the function
+// removeRecursively('path/to/directory')
+//   .then(() => console.log('Directory and all its contents have been removed recursively.'))
+//   .catch(console.error);
+
+
+const app = express();
+app.use(timeout('500s'));
+const upload = multer({ dest: 'uploads/' });
+app.post('/upload', upload.single('video'), (req, res) => {
+
+// const __filename = fileURLToPath(req.file.url); // get the resolved path to the file
+// const __dirname = path.dirname(__filename); // get the name of the directory
+
+
+  const videoPath = req.file.path;
+  const outputFilePath = `compressed_${req.file.originalname}`;
+  // Call the compression function here
+  compressVideo(videoPath, outputFilePath, res);
+});
+const compressVideo = (inputPath, outputPath, res) => {
+  ffmpeg(inputPath)
+    .output(path.join(__dirname, 'uploads', outputPath))
+    .videoCodec('libx264')
+    .size('30%')
+    .on('end', () => {
+      console.log('Compression completed!');
+    //  res.send('Video uploaded and compressed successfully!');
+      // return comporessed file to client
+      return res.status(200).sendFile(outputPath, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', outputPath);
+            // then remove al files 
+        }
+      });
+    })
+    .on('error', (err) => {
+      console.error('Compression failed:', err);
+      res.status(500).send('Compression failed.');
+    })
+    .run();
+};
+const PORT = process.env.PORT || 4444;
+let server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// server.setTimeout(5000000);
